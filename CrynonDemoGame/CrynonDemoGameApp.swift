1c9f3b7d3668cb733249f6191b5c4d71cee46065
KudeKube <Kuutti.taavitsainen@icloud.com>
2023-10-31 18:50:51 +0200

    Initial Commit

---
+++ CrynonDemoGame/CrynonDemoGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrynonDemoGameApp.swift
+//  CrynonDemoGame
+//
+//  Created by Kuutti Taavitsainen on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CrynonDemoGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
